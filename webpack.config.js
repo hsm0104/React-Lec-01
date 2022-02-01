105c77ee273847b5b44d7d9c5d53ae5ce3eef3af
Saemi Hong <sstyle940104@gmail.com>
2022-02-01 21:48:27 +0900

    add : Add babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 // node js file
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode : '',
-    entry: '', //which js file to start. input
-    output: {},
+    mode : 'development', //on Service / Developer mode
+    entry: './app.js', //which js file to start. input
+    output: {
+        // 1. which directory to use. 2. what name
+        path: path.resolve(__dirname, 'dist'), //using path built in node 
+        filename: "bundle.js"
+    },
 
     module: {
-
-    },
+        rules: [
+            {
+                test: /\.js$/, //only JS 
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader", //can deal JS only. Not Css, img...etc
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            }
+        }
+        ]
+    }, //transpiler setting
     plugins: [
-        
+        new HtmlWebpackPlugin({
+            title: 'settting Babel and WebPack',
+            template: 'index.html' //which html file to use?
+        })
     ]
 }
